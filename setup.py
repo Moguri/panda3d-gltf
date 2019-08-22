1ee7cb5a5e573444ba4d8dd2b4bb89ad13f239cb
Mitchell Stokes <mogurijin@gmail.com>
2019-08-21 18:42:59 -0700

    Move information from setup.py to setup.cfg
    Also add author and home-page metadata.
    
    Closes #16

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as readme_file:
-        return readme_file.read()
-
 setup(
-    name='panda3d-gltf',
     version='0.1.1',
-    description='glTF utilities for Panda3D',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-    ],
     keywords='panda3d gltf',
     packages=['gltf'],
-    include_package_data=True,
     install_requires=[
         'panda3d',
     ],
