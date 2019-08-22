f17f3fd4601aeff3c7a346eca0c35e2d70480702
Mitchell Stokes <mogurijin@gmail.com>
2019-08-21 19:08:38 -0700

    tests: Add basic unit tests for loading models

---
+++ tests/test_load.py
@@ -0,0 +1,37 @@
+import os
+
+import panda3d.core as p3d
+import pytest
+
+
+import gltf
+
+#pylint:disable=redefined-outer-name
+
+
+@pytest.fixture(scope='session')
+def showbase():
+    from direct.showbase.ShowBase import ShowBase
+    p3d.load_prc_file_data('', 'window-type none')
+    base = ShowBase()
+    gltf.patch_loader(base.loader)
+    return base
+
+@pytest.fixture
+def modelpath():
+    return p3d.Filename.from_os_specific(
+        os.path.join(
+            os.path.dirname(__file__),
+            'test.gltf'
+        )
+    )
+
+def test_load_single(showbase, modelpath):
+    showbase.loader.load_model(modelpath)
+
+
+def test_load_multiple(showbase, modelpath):
+    showbase.loader.load_model([modelpath, modelpath])
+    showbase.loader.load_model({modelpath, modelpath})
+    # doesn't work on Panda3D 1.10.4+
+    # showbase.loader.load_model((modelpath, modelpath))
