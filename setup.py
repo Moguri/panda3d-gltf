efe7f8a6d48cd96085ef5ffbe167098c4c0f2d5b
Stephen Imhoff <simhoff@costco.com>
2021-04-29 22:30:03 +0000

    change entrypoints to be from setup.cfg

--- setup.py
+++ setup.py
@@ -5,17 +5,5 @@ __version__ = ''
 exec(open('gltf/version.py').read())
 
 setup(
-    version=__version__,
-    entry_points={
-        'console_scripts': [
-            'gltf2bam=gltf.cli:main'
-        ],
-        'gui_scripts': [
-            'gltf-viewer=gltf.viewer:main'
-        ],
-        'panda3d.loaders': [
-            'gltf=gltf.loader:GltfLoader',
-            'glb=gltf.loader:GltfLoader',
-        ],
-    },
+    version=__version__
 )
