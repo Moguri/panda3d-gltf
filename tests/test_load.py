e554617266f59e1bb2e3d8e989366dd6c81b29a5
Mitchell Stokes <mogurijin@gmail.com>
2022-07-23 14:19:36 -0700

    tests: Disable model cache

--- tests/test_load.py
+++ tests/test_load.py
@@ -12,9 +12,15 @@ def showbase():
     prcdata = (
         'window-type none\n'
         'audio-library-name null\n'
+        'model-cache-dir\n'
     )
     p3d.load_prc_file_data('', prcdata)
     base = ShowBase()
+
+    type_registry = p3d.LoaderFileTypeRegistry.get_global_ptr()
+    ftype = type_registry.get_type_from_extension('gltf')
+    assert ftype.getName() == 'Python loader'
+
     return base
 
 @pytest.fixture
@@ -28,7 +34,6 @@ def modelpath():
 
 def test_load_single(showbase, modelpath):
     showbase.loader.load_model(modelpath)
-
 
 def test_load_multiple(showbase, modelpath):
     showbase.loader.load_model([modelpath, modelpath])
