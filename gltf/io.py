64283558d3f7a9a75ec9c201630849c620fae1bb
Mitchell Stokes <mogurijin@gmail.com>
2022-12-27 00:37:17 -0800

    io: Separate glb file loading from decoding
    Fixes #110

--- gltf/io.py
+++ gltf/io.py
@@ -10,40 +10,45 @@ def is_glb_file(filepath):
             return False
 
 
-def read_glb_file(filepath):
+def decode_glb(data):
     def read_glb_chunk(glbfile):
         chunk_size, = struct.unpack('<I', glbfile.read(4))
         chunk_type = glbfile.read(4)
         chunk_data = glbfile.read(chunk_size)
         return chunk_type, chunk_data
 
-    with open(filepath, 'rb') as glbfile:
-        if glbfile.read(4) != b'glTF':
-            raise RuntimeError('attempted to load non-glb file as glb')
+    if data.read(4) != b'glTF':
+        raise RuntimeError('attempted to load non-glb file as glb')
 
-        version, = struct.unpack('<I', glbfile.read(4))
-        if version != 2:
-            raise RuntimeError(
-                f'Only GLB version 2 is supported, file is version {version}'
-            )
+    version, = struct.unpack('<I', data.read(4))
+    if version != 2:
+        raise RuntimeError(
+            f'Only GLB version 2 is supported, file is version {version}'
+        )
 
-        length, = struct.unpack('<I', glbfile.read(4))
+    length, = struct.unpack('<I', data.read(4))
 
-        chunk_type, chunk_data = read_glb_chunk(glbfile)
-        assert chunk_type == b'JSON'
-        gltf_data = json.loads(chunk_data.decode('utf-8'))
+    chunk_type, chunk_data = read_glb_chunk(data)
+    assert chunk_type == b'JSON'
+    gltf_data = json.loads(chunk_data.decode('utf-8'))
 
-        if glbfile.tell() < length:
-            chunk_type, chunk_data = read_glb_chunk(glbfile)
-            assert chunk_type == b'BIN\000'
-            if not 'buffers' not in gltf_data:
-                gltf_data['buffers'] = []
-            gltf_data['buffers'].insert(0, {
-                'uri': '_glb_bin',
-                '_glb_bin': chunk_data,
-            })
+    if data.tell() < length:
+        chunk_type, chunk_data = read_glb_chunk(data)
+        assert chunk_type == b'BIN\000'
+        if not 'buffers' not in gltf_data:
+            gltf_data['buffers'] = []
+        gltf_data['buffers'].insert(0, {
+            'uri': '_glb_bin',
+            '_glb_bin': chunk_data,
+        })
 
     return gltf_data
+
+
+
+def read_glb_file(filepath):
+    with open(filepath, 'rb') as glbfile:
+        return decode_glb(glbfile)
 
 
 def read_gltf_file(filepath):
