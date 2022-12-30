f92a98f6571631c3a900f5aa0c93a4d01b9f3735
Mitchell Stokes <mogurijin@gmail.com>
2022-12-29 22:03:24 -0800

    tests: Add test for loading external textures

---
+++ tests/test_converter.py
@@ -0,0 +1,25 @@
+import panda3d.core as p3d
+from direct.actor.Actor import Actor
+
+import gltf
+
+
+def load_test_asset(modelroot, assetname) -> p3d.NodePath:
+    return p3d.NodePath(gltf.load_model(p3d.Filename(modelroot, assetname)))
+
+def test_external_texture(modelroot):
+    model = load_test_asset(modelroot, 'BoxTextured.gltf')
+
+    textures = model.find_all_textures('CesiumLogoFlat')
+    assert textures
+
+    texture: p3d.Texture = textures[0]
+    assert texture.filename == 'CesiumLogoFlat.png'
+
+def test_simple_anim(modelroot):
+    model = load_test_asset(modelroot, 'Fox.glb')
+    assert model
+
+    actor = Actor(p3d.NodePath(model))
+
+    assert len(actor.get_anim_names()) == 3
